8cefe7a8c410449bbf7dc03be72bfce6d216188d
anaramosicai <anaramosicai@gmail.com>
2026-02-02 18:02:14 +0000

    Upload of the Project

---
+++ grupo5/src/test/java/edu/comillas/icai/gitt/pat/spring/grupo5/Grupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.comillas.icai.gitt.pat.spring.grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
