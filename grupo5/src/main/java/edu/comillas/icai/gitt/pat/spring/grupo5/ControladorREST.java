b095a9e8da881b071357a50e6958b756eee1f37c
Felicia Huynh <felicia.huynh.6021@student.lu.se>
2026-02-09 20:10:01 +0100

    security, controlador, nombreRol, Pista and Rol

---
+++ grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/ControladorREST.java
@@ -0,0 +1,114 @@
+package edu.comillas.icai.gitt.pat.spring.grupo5;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RestController
+public class ControladorREST {
+
+    Map<Integer, Pista> pistas = new ConcurrentHashMap<Integer, Pista>();
+    private int idPistaContador = 0;
+    private static final int N = 1000;
+
+
+    @PostMapping("/pistaPadel/courts")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ADMIN')")
+    public Pista crea(@RequestBody Pista pista) {
+
+        boolean nombreDuplicado = pistas.values().stream()
+                .anyMatch(p -> p.nombre().equalsIgnoreCase(pista.nombre()));
+
+        if (nombreDuplicado) {
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "Otra pista con la igual nombre"
+            );
+        }
+
+        if (idPistaContador > N){
+            throw new ResponseStatusException(
+                    HttpStatus.CONFLICT,
+                    "idPista es major de N"
+            );
+        }
+
+        int idPista = idPistaContador++;
+
+        Pista pistaNuevo = new Pista(
+                idPista,
+                pista.nombre(),
+                pista.ubicacion(),
+                pista.precioHora(),
+                pista.activa(),
+                pista.fechaAlta()
+        );
+        pistas.put(idPista, pistaNuevo);
+        return pistaNuevo;
+    }
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @GetMapping("/pistaPadel/courts")
+    public List<Pista> pistas() {
+        ArrayList<Pista> pistaList = new ArrayList<>();
+        for (Pista p : pistas.values()){
+            pistaList.add(p);
+        }
+        return pistaList;
+    }
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @GetMapping("/pistaPadel/courts/{courtId}")
+    public Pista obtenerDetalle(@PathVariable int courtId){
+        Pista pista = pistas.get(courtId);
+        if(pista == null){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "no pista con este id"
+            );
+        }
+
+        return pista;
+    }
+
+    @PatchMapping("/pistaPadel/courts/{courtId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Pista modificarPista(@PathVariable int courtId, @RequestBody Pista pistaMod){
+        Pista pista = pistas.get(courtId);
+        if (pista == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        Pista pistaActualizada = new Pista(
+                courtId,
+                pista.nombre(),
+                pistaMod.ubicacion(),
+                pistaMod.precioHora(),
+                pistaMod.activa(),
+                pistaMod.fechaAlta());
+
+        pistas.put(courtId, pistaActualizada);
+        return pistaActualizada;
+    }
+
+
+    @DeleteMapping("/pistaPadel/courts/{courtId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public void borrar(@PathVariable int courtId){
+        Pista pista = pistas.get(courtId);
+        if(pista == null){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "no pista con este id"
+            );
+        }
+        pistas.remove(courtId);
+    }
+}
---
+++ grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/NomreRol.java
@@ -0,0 +1,6 @@
+package edu.comillas.icai.gitt.pat.spring.grupo5;
+
+public enum NomreRol {
+    USER,
+    ADMIN
+}
