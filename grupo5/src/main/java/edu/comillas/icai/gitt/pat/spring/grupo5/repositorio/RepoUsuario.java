7a8bdf6698f89d09dfc11ef1a8fca2d27caa7802
MartinaOD <202302674@alu.comillas.edu>
2026-03-15 22:30:39 +0100

    update RepoUsuario

--- grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/repositorio/RepoUsuario.java
+++ grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/repositorio/RepoUsuario.java
@@ -6,4 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface RepoUsuario extends CrudRepository<Usuario, Long> {
     Usuario findByEmail(String email);
     Usuario findByNombre(String nombre);
+    // Crud tiene implícito: Iterable<Usuario> findAll();
+    boolean existsByEmailAndIdNot(String email, Long id);
 }
