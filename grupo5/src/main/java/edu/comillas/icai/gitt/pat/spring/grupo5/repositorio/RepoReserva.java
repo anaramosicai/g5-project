8d151a0c4e6e6593ecf766ea45d95072bb26282e
MartinaOD <202302674@alu.comillas.edu>
2026-03-18 11:03:26 +0100

    Format and clean up RepoReserva.java

--- grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/repositorio/RepoReserva.java
+++ grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/repositorio/RepoReserva.java
@@ -1,7 +1,7 @@
 package edu.comillas.icai.gitt.pat.spring.grupo5.repositorio;
 
 import edu.comillas.icai.gitt.pat.spring.grupo5.entity.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
