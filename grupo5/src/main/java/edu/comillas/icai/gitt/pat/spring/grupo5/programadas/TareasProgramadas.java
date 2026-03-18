aa8bd5a960e0907629e5cdd305cea6b56987ab9c
MartinaOD <202302674@alu.comillas.edu>
2026-03-18 01:46:29 +0100

    arreglo programadas

--- grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/programadas/TareasProgramadas.java
+++ grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/programadas/TareasProgramadas.java
@@ -24,22 +24,18 @@ public class TareasProgramadas {
     public void remindPista() {
         logger.info("Me ejecuto cada día a las 2 AM");
 
-        */
-/* Mandar correo a usuarios que tienen pista reservada para ese día *//*
+        /* Mandar correo a usuarios que tienen pista reservada para ese día */
 
-
-        servicioReserva.enviarRecordatorioDia(); // Esta clase debemos implementarla en Servicio
+        //servicioReserva.enviarRecordatorioDia(); // Esta clase debemos implementarla en Servicio
     }
 
     @Scheduled(cron = "0 0 0 1 * *") // Se ejecutará justo al empezar el día 1
     public void showDisponibilidad() {
         logger.info("Me ejecuto el día 1 de cada mes");
 
-        */
-/* Mandar correo a todos los usuarios con las pistas y los horarios disponibles *//*
+        /* Mandar correo a todos los usuarios con las pistas y los horarios disponibles */
 
-
-        servicioPista.enviarDisponibilidadMensual(); // Esta clase debemos implementarla en Servicio
+        //servicioPista.enviarDisponibilidadMensual(); // Esta clase debemos implementarla en Servicio
     }
 
 }
