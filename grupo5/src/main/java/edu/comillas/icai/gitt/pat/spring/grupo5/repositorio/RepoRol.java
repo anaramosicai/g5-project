064e456f2c240c428fae6e98a288d523ed019071
Antonio Lafont Carrasco <202302135@alu.comillas.edu>
2026-03-18 01:19:38 +0100

    Delete grupo5 directory

--- grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/repositorio/RepoRol.java
+++
@@ -1,8 +0,0 @@
-package edu.comillas.icai.gitt.pat.spring.grupo5.repositorio;
-
-import edu.comillas.icai.gitt.pat.spring.grupo5.entity.Rol;
-import edu.comillas.icai.gitt.pat.spring.grupo5.entity.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RepoRol extends CrudRepository<Rol, Long> {
-}
