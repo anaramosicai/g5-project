73ad0a0710659fa3530837dd21ab5e5e29995989
MartinaOD <202302674@alu.comillas.edu>
2026-04-29 00:06:43 +0200

    Enable scheduling in Grupo5Application

--- BACKEND/grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/Grupo5Application.java
+++ BACKEND/grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/Grupo5Application.java
@@ -3,6 +3,7 @@ package edu.comillas.icai.gitt.pat.spring.grupo5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableScheduling
 @SpringBootApplication
 public class Grupo5Application {
 
