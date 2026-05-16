1dc5d3a775021d52753a0c9540289f94ee6093f2
MartinaOD <202302674@alu.comillas.edu>
2026-05-17 01:05:28 +0200

    Delete BACKEND directory

--- BACKEND/grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/model/DisponibilidadResponse.java
+++
@@ -1,5 +0,0 @@
-package edu.comillas.icai.gitt.pat.spring.grupo5.model;
-
-import java.time.LocalDate;
-
-public record DisponibilidadResponse(LocalDate fecha, Long courtId, boolean disponible, String mensaje) {}
--- BACKEND/grupo5/src/main/java/edu/comillas/icai/gitt/pat/spring/grupo5/model/ModeloUsuarioIncorrecto.java
+++
@@ -1,4 +0,0 @@
-package edu.comillas.icai.gitt.pat.spring.grupo5.model;
-
-public record ModeloUsuarioIncorrecto (String error, String campo, Object valor){
-}
